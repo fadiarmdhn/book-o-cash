6f1232cebc28d9e351078c2e567683535fc606ce
Indah Puspita <47053902+indahpuspitaa17@users.noreply.github.com>
2020-11-05 00:17:21 +0700

    [indah] add inventory

--- pages/sales.vue
+++ pages/sales.vue
@@ -3,8 +3,9 @@
     <div class="col-md-12"><order-in-process></order-in-process></div>
   </div>
 </template>
+
 <script>
-import OrderInProcess from '../components/Sales/OrderInProcess'
+import OrderInProcess from '../components/Sales/OrderInProcess';
 export default {
   name: 'sales',
   components: {
