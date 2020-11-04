1ab11aad0cbe99496b7f8ebe0fd72b03789f334f
Indah Puspita <47053902+indahpuspitaa17@users.noreply.github.com>
2020-11-04 23:41:55 +0700

    [indah] add order in process

---
+++ components/Sales/OrderInProcess.vue
@@ -0,0 +1,171 @@
+<template>
+  <div class="row">
+    <div class="col-md-12">
+      <card card-body-classes="table-full-width">
+        <h3 slot="header" class="title">Sales Record</h3>
+        <base-button 
+          class="animation-on-hover" 
+          size="sm" 
+          type="info">Add Order</base-button>
+        <el-table 
+        :data="tableData.filter(data => !search || data.name.toLowerCase().includes(search.toLowerCase()))"
+        style="width: 100%">
+          <el-table-column
+            sortable
+            label="Order ID"
+            property="orderId"
+          ></el-table-column>
+          <el-table-column
+            sortable
+            label="Name"
+            property="name"
+          ></el-table-column>
+          <el-table-column
+            sortable
+            label="Order Type"
+            property="type"
+          ></el-table-column>
+          <el-table-column
+            sortable
+            align="center"
+            header-align="center"
+            label="Table Number"
+            property="tableNumber"
+          ></el-table-column>
+          <el-table-column
+            sortable
+            align="center"
+            header-align="center"
+            label="Cashier"
+            property="cashier"
+          ></el-table-column>
+          <el-table-column
+            sortable
+            align="center"
+            header-align="center"
+            label="Status"
+            property="status"
+          ></el-table-column>
+          <el-table-column>
+            <div slot-scope="scope">
+                 <base-button 
+                    size="sm"
+                    type="warning"
+                    @click="handleDetail(scope.$index, scope.row)"
+                  > Edit</base-button>
+            </div>
+          </el-table-column>
+          <el-table-column>
+            <div slot-scope="scope">
+                 <base-button 
+                    class="animation-on-hover"
+                    size="sm"
+                    type="default"
+                    @click="handleDetail(scope.$index, scope.row)"
+                  > Finish</base-button>
+            </div>
+          </el-table-column>
+        </el-table>  
+        <div >
+          <base-pagination 
+            :page-count="5" 
+            v-model="defaultPagination"></base-pagination>
+        </div>
+      </card>
+    </div>
+  </div>
+</template>
+<script>
+import { Table, TableColumn } from 'element-ui';
+import {BasePagination} from '@/components'
+
+export default {
+  components: {
+    [Table.name]: Table,
+    [TableColumn.name]: TableColumn,
+    BasePagination
+  },
+  data() {
+    return {
+      defaultPagination: 1,
+      tableData: [
+        {
+          id: 1,
+          orderId: '#0001',
+          tableNumber: '08',
+          name: 'Aan',
+          type: 'Dine-in',
+          cashier: 'Bambang',
+          status: 'In process'
+        },
+        {
+          id: 2,
+          orderId: '#0002',
+          tableNumber: '03',
+          name: 'Ahmad',
+          type: 'Dine-in',
+          cashier: 'Bambang',
+          status: 'In process'
+        },
+        {
+          id: 3,
+          orderId: '#0003',
+          tableNumber: '42',
+          name: 'Bob',
+          type: 'Dine-in',
+          cashier: 'Bambang',
+          status: 'In process'
+        },
+        {
+          id: 4,
+          orderId: '#0004',
+          tableNumber: '-',
+          name: 'Yuwono',
+          type: 'Takeaway',
+          cashier: 'Bambang',
+          status: 'In process'
+        },
+        {
+          id: 5,
+          orderId: '#0005',
+          tableNumber: '-',
+          name: 'Fadia',
+          type: 'Takeaway',
+          cashier: 'Bambang',
+          status: 'In process'
+        },
+        {
+          id: 6,
+          orderId: '#0006',
+          tableNumber: '17',
+          name: 'Indah',
+          type: 'Dine-in',
+          cashier: 'Bambang',
+          status: 'In process'
+        },
+        {
+          id: 7,
+          orderId: '#0007',
+          tableNumber: '-',
+          name: 'Puspita',
+          type: 'Takeaway',
+          cashier: 'Bambang',
+          status: 'In process'
+        }
+      ],
+      search: '',
+    };
+  },
+  methods: {
+      handleDetail(index, row) {
+        console.log(index, row);
+      },
+    }
+};
+</script>
+
+<style>
+.table-transparent {
+  background-color: transparent !important;
+}
+</style>
--- pages/sales.vue
+++ pages/sales.vue
@@ -1,50 +1,15 @@
 <template>
   <div class="row">
-    <div class="col-md-12">
-      <div class="card">
-        <div class="card-header">
-          <h5 class="title">100 Awesome Nucleo Icons</h5>
-          <p class="category">
-            Handcrafted by our friends from
-            <a href="https://nucleoapp.com/?ref=1712">NucleoApp</a>
-          </p>
-        </div>
-        <div class="card-body all-icons">
-          <div class="row">
-              
-            <div
-              class="font-icon-list col-lg-2 col-md-3 col-sm-4 col-xs-6 col-xs-6"
-            >
-              <div class="font-icon-detail">
-                <i class="tim-icons icon-simple-remove"></i>
-                <p>icon-simple-remove</p>
-              </div>
-            </div>
-            <div
-              class="font-icon-list col-lg-2 col-md-3 col-sm-4 col-xs-6 col-xs-6"
-            >
-              <div class="font-icon-detail">
-                <i class="tim-icons icon-simple-add"></i>
-                <p>icon-simple-add</p>
-              </div>
-            </div>
-            <div
-              class="font-icon-list col-lg-2 col-md-3 col-sm-4 col-xs-6 col-xs-6"
-            >
-              <div class="font-icon-detail">
-                <i class="tim-icons icon-simple-delete"></i>
-                <p>icon-simple-delete</p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+    <div class="col-md-12"><order-in-process></order-in-process></div>
   </div>
 </template>
 <script>
+import OrderInProcess from '../components/Sales/OrderInProcess'
 export default {
-  name: 'sales'
+  name: 'sales',
+  components: {
+    OrderInProcess
+  }
 };
 </script>
 <style></style>
